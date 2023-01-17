5270d20c1e82794d3b728971312454a656a309c1
rdnrn <rodina.arina00@gmail.com>
2023-01-17 15:28:54 +0300

    add solution

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHW1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanHW2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Сравнение HelloWorld: " + (beanHW1 == beanHW2));
+        System.out.println("Сравнение Cat: " + (beanCat1 == beanCat2));
     }
 }
